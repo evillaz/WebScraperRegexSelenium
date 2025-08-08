d3ba7ff64efd443a24c7869a09ac24d1d65f1d25
evillaz <evillaz123@gmail.com>
2025-08-08 15:41:30 -0500

    Initialize C# webscrapping project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebScraperRegexSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
