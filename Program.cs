47ad0b67aa7f97dd8689bc204049ccd904faa850
evillaz <evillaz123@gmail.com>
2025-08-08 17:17:37 -0500

    webscrapper

--- Program.cs
+++ Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace WebScraperRegexSelenium
 {
-    class Program
+  class Product
+  {
+    public string Name { get; set; }
+    public string Price { get; set; }
+  }
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      var options = new ChromeOptions();
+      options.AddArgument("--headless");
+      options.AddArgument("--no-sandbox");
+      using var driver = new ChromeDriver(options);
+
+      driver.Navigate().GoToUrl("https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops");
+      Thread.Sleep(3000);
+      var products = new List<Product>();
+
+      var productElements = driver.FindElements(By.CssSelector(".product-wrapper.card-body"));
+      foreach (var productElement in productElements)
+      {
+        try
         {
-            Console.WriteLine("Hello World!");
+          var price = productElement.FindElement(By.CssSelector("span[itemprop='price']")).Text;
+
+          // Extract name
+          var name = productElement.FindElement(By.CssSelector("a.title")).Text;
+
+          products.Add(new Product
+          {
+            Name = name,
+            Price = price
+          });
         }
+        catch (NoSuchElementException)
+        {
+          // Skip any element that doesn’t match
+        }
+      }
+
+      // Output all
+      foreach (var product in products)
+      {
+        Console.WriteLine($"Name: {product.Name} | Price: {product.Price}");
+      }
+
+      driver.Quit();
     }
+  }
 }
